9b22cdac240941dedfce6afb1bf9deb65149e4bd
Rushil13579 <61579586+Rushil13579@users.noreply.github.com>
2021-03-01 18:25:26 +0530

    Add files via upload

--- src/Rushil13579/EasyTP/EasyTP.php
+++ src/Rushil13579/EasyTP/EasyTP.php
@@ -8,29 +8,44 @@ use pocketmine\plugin\PluginBase;
 
 use pocketmine\command\{Command, CommandSender};
 
-use pocketmine\utils\Config;
+use pocketmine\utils\{Config, TextFormat as C};
 
 class EasyTP extends PluginBase {
+
+  public $cfg;
 
   public $tp = [];
   public $tpcd = [];
 
+  public const PREFIX = '§3[§bEasyTP§3]';
+
   public function onEnable(){
     $this->saveDefaultConfig();
     $this->getResource('config.yml');
+
+    $this->cfg = $this->getConfig();
+
+    $this->versionCheck();
+  }
+
+  public function versionCheck(){
+    if($this->cfg->get('version') !== '1.1.0'){
+      $this->getLogger()->warning(self::PREFIX . ' §cThe current configuration file is outdated. Please delete it and restart the server to install the latest configuration file!');
+      $this->getServer()->getPluginManager()->disablePlugin($this);
+    }
   }
 
   public function levelCheck($player){
     $level = $player->getLevel()->getName();
 
-    if(in_array($level, $this->getConfig()->get('no-tp-levels'))){
-      $player->sendMessage('§cThis command is banned in this level');
+    if(in_array($level, $this->cfg->get('blacklist-levels'))){
+      $player->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('blacklisted-level-msg'))));
       return null;
     }
 
-    foreach(array_keys($this->getConfig()->get('blacklist-contains')) as $key){
-      if(strpos($this->getConfig()->get('blaclist-contains'), $player->getLevel()->getName())){
-        $player->sendMessage('§cThis command is banned in this level');
+    foreach(array_keys($this->cfg->get('blacklist-contains')) as $key){
+      if(strpos($this->cfg->get('blaclist-contains'), $player->getLevel()->getName())){
+        $player->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('blacklisted-level-msg'))));
         return null;
       }
     }
@@ -38,10 +53,10 @@ class EasyTP extends PluginBase {
   }
 
   public function cooldownCheck($player){
-    $tpcd = $this->getConfig()->get('tp-cooldown');
+    $tpcd = $this->cfg->get('tp-cooldown');
     if(!is_numeric($tpcd)){
-      $this->getLogger()->warning('§ctp-cooldown isn\'t numeric');
-      $player->sendMessage('§cError, please contact server administrators');
+      $this->getLogger()->warning(self::PREFIX . ' §ctp-cooldown isn\'t numeric');
+      $player->sendMessage(self::PREFIX . ' §cError, please contact server administrators');
       return null;
     }
 
@@ -50,7 +65,7 @@ class EasyTP extends PluginBase {
     } else {
       if(time() < $this->tpcd[$player->getName()]){
         $rem = $this->tpcd[$player->getName()] - time();
-        $player->sendMessage("§cYou are on cooldown for $rem seconds");
+        $player->sendMessage(C::colorize(str_replace(['{PREFIX}', '{REMAINING}'], [self::PREFIX, $rem], $this->cfg->get('tp-cooldown-msg'))));
         return null;
       } else {
         unset($this->tpcd[$player->getName()]);
@@ -73,14 +88,14 @@ class EasyTP extends PluginBase {
 
     if($type == 'tpa'){
       $this->tp[$player->getName()] = ['tpa', $target->getName()];
-      $player->sendMessage("§aYou have sent a tpa request to §6" . $target->getName());
-      $target->sendMessage("§aYou have received a tpa request from §6" . $player->getName() . ". §aYou can accept the request using /tpaccept " . $player->getName());
+      $player->sendMessage(C::colorize(str_replace(['{PREFIX}', '{RECEIVER}'], [self::PREFIX, $target->getName()], $this->cfg->get('tpa-request-sender-msg'))));
+      $target->sendMessage(C::colorize(str_replace(['{PREFIX}', '{REQUESTER}'], [self::PREFIX, $target->getName()], $this->cfg->get('tpa-request-receiver-msg'))));
     }
 
     if($type == 'tpahere'){
       $this->tp[$player->getName()] = ['tpahere', $target->getName()];
-      $player->sendMessage("§aYou have sent a tpahere request to §6" . $target->getName());
-      $target->sendMessage("§aYou have received a tpahere request from §6" . $player->getName() . ". §aYou can accept the request using /tpaccept " . $player->getName());
+      $player->sendMessage(C::colorize(str_replace(['{PREFIX}', '{RECEIVER}'], [self::PREFIX, $target->getName()], $this->cfg->get('tpahere-request-sender-msg'))));
+      $target->sendMessage(C::colorize(str_replace(['{PREFIX}', '{REQUESTER}'], [self::PREFIX, $target->getName()], $this->cfg->get('tpahere-request-receiver-msg'))));
     }
   }
 
@@ -91,12 +106,12 @@ class EasyTP extends PluginBase {
     }
 
     if(!isset($this->tp[$target->getName()])){
-      $player->sendMessage("§cYou don't have a tp request from " . $target->getName());
+      $player->sendMessage(C::colorize(str_replace(['{PREFIX}', '{TARGET}'], [self::PREFIX, $target->getName()], $this->cfg->get('no-tp-request-exists-msg'))));
       return null;
     }
 
     if($this->tp[$target->getName()][1] !== $player->getName()){
-      $player->sendMessage("§cYou don't have a tp request from " . $target->getName());
+      $player->sendMessage(C::colorize(str_replace(['{PREFIX}', '{TARGET}'], [self::PREFIX, $target->getName()], $this->cfg->get('no-tp-request-exists-msg'))));
       return null;
     }
 
@@ -105,43 +120,43 @@ class EasyTP extends PluginBase {
       if($tptype == 'tpa'){
         unset($this->tp[$target->getName()]);
         $target->teleport($player);
-        $player->sendMessage('§aTeleporting...');
+        $player->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('teleporting-msg'))));
       }
       if($tptype == 'tpahere'){
         unset($this->tp[$target->getName()]);
         $player->teleport($target);
-        $target->sendMessage('§aTeleporting...');
+        $target->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('teleporting-msg'))));
       }
     }
     if($type == 'tpdeny'){
       unset($this->tp[$target->getName()]);
-      $player->sendMessage('§cYou have denied a tp request from ' . $target->getName());
-      $target->sendMessage("§c" . $player->getName() . " has denied your tp request");
+      $player->sendMessage(C::colorize(str_replace(['{PREFIX}', '{REQUESTER}'], [self::PREFIX, $target->getName()], $this->cfg->get('request-denied-receiver-msg'))));
+      $target->sendMessage(C::colorize(str_replace(['{PREFIX}', '{RECEIVER}'], [self::PREFIX, $player->getName()], $this->cfg->get('request-denied-requester-msg'))));
     }
   }
 
   public function onCommand(CommandSender $s, Command $cmd, String $label, Array $args) : bool {
 
     switch($cmd->getName()){
-      case "tpa":
+      case 'tpa':
 
       if(!$s instanceof Player){
-        $s->sendMessage('§cPlease use this command in-game');
+        $s->sendMessage(self::PREFIX . ' §cPlease use this command in-game');
         return false;
       }
 
       if(!isset($args[0])){
-        $s->sendMessage('§cUsage: /tpa [player]');
+        $s->sendMessage(self::PREFIX . ' §cUsage: /tpa [player] | /tpask [player]');
         return false;
       }
 
       if($this->getServer()->getPlayer($args[0]) === null or !$this->getServer()->getPlayer($args[0])->isOnline()){
-        $s->sendMessage('§cPlayer not found');
+        $s->sendMessage(self::PREFIX . ' §cPlayer not found');
         return false;
       }
 
       if($this->getServer()->getPlayer($args[0]) == $s->getName()){
-        $s->sendMessage('§cYou cannot send a tp request to your self');
+        $s->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('no-self-tp-request'))));
         return false;
       }
 
@@ -151,25 +166,25 @@ class EasyTP extends PluginBase {
     }
 
     switch($cmd->getName()){
-      case "tpahere":
+      case 'tpahere':
 
       if(!$s instanceof Player){
-        $s->sendMessage('§cPlease use this command in-game');
+        $s->sendMessage(self::PREFIX . ' §cPlease use this command in-game');
         return false;
       }
 
       if(!isset($args[0])){
-        $s->sendMessage('§cUsage: /tpahere [player]');
+        $s->sendMessage(self::PREFIX . ' §cUsage: /tpahere [player] | /tph [player]');
         return false;
       }
 
       if($this->getServer()->getPlayer($args[0]) === null or !$this->getServer()->getPlayer($args[0])->isOnline()){
-        $s->sendMessage('§cPlayer not found');
+        $s->sendMessage(self::PREFIX . ' §cPlayer not found');
         return false;
       }
 
       if($this->getServer()->getPlayer($args[0]) == $s->getName()){
-        $s->sendMessage('§cYou cannot send a tp request to your self');
+        $s->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('no-self-tp-request'))));
         return false;
       }
 
@@ -179,20 +194,20 @@ class EasyTP extends PluginBase {
     }
 
     switch($cmd->getName()){
-      case "tpaccept":
+      case 'tpaccept':
 
       if(!$s instanceof Player){
-        $s->sendMessage('§cPlease use this command in-game');
+        $s->sendMessage(self::PREFIX . ' §cPlease use this command in-game');
         return false;
       }
 
       if(!isset($args[0])){
-        $s->sendMessage('§cUsage: /tpaccept [player] | /tpyes [player]');
+        $s->sendMessage(self::PREFIX . ' §cUsage: /tpaccept [player] | /tpyes [player] | /tpok [player]');
         return false;
       }
 
       if($this->getServer()->getPlayer($args[0]) === null or !$this->getServer()->getPlayer($args[0])->isOnline()){
-        $s->sendMessage('§cPlayer not found');
+        $s->sendMessage(self::PREFIX . ' §cPlayer not found');
         return false;
       }
 
@@ -202,26 +217,75 @@ class EasyTP extends PluginBase {
     }
 
     switch($cmd->getName()){
-      case "tpdeny":
+      case 'tpdeny':
 
       if(!$s instanceof Player){
-        $s->sendMessage('§cPlease use this command in-game');
+        $s->sendMessage(self::PREFIX . ' §cPlease use this command in-game');
         return false;
       }
 
       if(!isset($args[0])){
-        $s->sendMessage('§cUsage: /tpdeny [player] | /tpno [player]');
+        $s->sendMessage(self::PREFIX . ' §cUsage: /tpdeny [player] | /tpno [player] | /tpdecline [player]');
         return false;
       }
 
       if($this->getServer()->getPlayer($args[0]) === null or !$this->getServer()->getPlayer($args[0])->isOnline()){
-        $s->sendMessage('§cPlayer not found');
+        $s->sendMessage(self::PREFIX . ' §cPlayer not found');
         return false;
       }
 
       $t = $this->getServer()->getPlayer($args[0]);
       $type = 'tpdeny';
       $this->tpAnswer($s, $t, $type);
+    }
+
+    switch($cmd->getName()){
+      case 'tphere':
+
+      if(!$s instanceof Player){
+        $s->sendMessage(self::PREFIX . ' §cPlease use this command in-game');
+        return false;
+      }
+
+      if(!$s->hasPermission('easytp.command.tphere') and !$s->hasPermission('easytp.command.*')){
+        $s->sendMessage(self::PREFIX . ' §cYou do not have permission to use this command');
+        return false;
+      }
+
+      if(!isset($args[0])){
+        $s->sendMessage(self::PREFIX . ' §cUsage: /tphere [player]');
+        return false;
+      }
+
+      if($this->getServer()->getPlayer($args[0]) === null){
+        $s->sendMessage(self::PREFIX . ' §cPlayer not found');
+        return false;
+      }
+
+      $player = $this->getServer()->getPlayer($args[0]);
+
+      $player->teleport($s);
+      $s->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('teleporting-msg'))));
+    }
+
+    switch($cmd->getName()){
+      case 'tpall':
+
+      if(!$s instanceof Player){
+        $s->sendMessage(self::PREFIX . ' §cPlease use this command in-game');
+        return false;
+      }
+
+      if(!$s->hasPermission('easytp.command.tpall') and !$s->hasPermission('easytp.command.*')){
+        $s->sendMessage(self::PREFIX . ' §cYou do not have permission to use this command');
+        return false;
+      }
+
+      foreach($this->getServer()->getOnlinePlayers() as $player){
+        $player->teleport($s);
+      }
+
+      $s->sendMessage(C::colorize(str_replace('{PREFIX}', self::PREFIX, $this->cfg->get('teleporting-msg'))));
     }
     return true;
   }
